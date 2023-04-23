643aff964f6376c260726b7e5f2938d7e55b184f
Heyamn <aman2000tailor29@gmail.com>
2023-04-24 00:21:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+import Main from './Components/Main';
+
+function App() {
+  return (
+    <div style={{
+      backgroundColor: '#121727'
+    }}>
+      <Navbar/>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
